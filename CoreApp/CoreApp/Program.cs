2b9c02993b001f73ab4eaea8611afda46b629fec
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2024-06-08 19:27:03 +0300

    Exp

---
+++ CoreApp/CoreApp/Program.cs
@@ -0,0 +1,24 @@
+using CoreApp;
+
+public class Program
+{
+    /// <summary>
+    ///  Точка входа - метод Main
+    /// </summary>
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    /// <summary>
+    /// Статический метод, создающий и настраивающий IHostBuilder -
+    /// объект, который в свою очередь создает хост для развертывания нашего приложения
+    /// </summary>
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+            webBuilder.UseWebRoot("Views");
+        });
+}
---
+++ CoreApp/CoreApp/Startup.cs
@@ -0,0 +1,41 @@
+﻿using CoreApp.Middlewares;
+
+namespace CoreApp
+{
+    public class Startup
+    {
+        IWebHostEnvironment _env;
+
+        //public Startup(IWebHostEnvironment env)
+        //{
+        //    _env = env;
+        //}
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment() || env.IsStaging())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.UseMiddleware<LoggingMiddleware>();
+
+            app.UseStatusCodePages();
+
+            Console.WriteLine($"Launching project from: {env.ContentRootPath}");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context => { await context.Response.WriteAsync($"Hellow world {env.EnvironmentName} {env.ApplicationName}"); });
+            });
+        }
+    }
+}
